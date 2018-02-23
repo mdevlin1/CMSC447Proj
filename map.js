ccda54028e8e0ac3f018e8269045b9087420b13d
Mitchell Devlin <mitchell.r.devlin@gmail.com>
2018-02-23 04:55:26 -0500

    external js and css

---
+++ map.js
@@ -0,0 +1,40 @@
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.255, lng: -76.710},
+    zoom: 15,
+    size: "800x800"
+});
+}
+
+function umbc() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.255, lng: -76.710},
+    zoom: 15,
+    size: "800x800"
+});
+}
+
+function london() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 51.507, lng: -0.127},
+    zoom: 10,
+    size: "800x800"
+});
+}
+
+function nyc() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.712, lng: -74.005},
+    zoom: 10,
+    size: "800x800"
+});
+}
+
+function la() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 34.052, lng: -118.243},
+    zoom: 10,
+    size: "800x800"
+});
+}
